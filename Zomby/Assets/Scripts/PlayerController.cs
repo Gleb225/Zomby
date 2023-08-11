1c694890460d162f251f37bb0752aa24a4b267a6
Gleb225 <78496118+Gleb225@users.noreply.github.com>
2023-08-11 14:35:40 +0300

    s
    s

--- Zomby/Assets/Scripts/PlayerController.cs
+++ Zomby/Assets/Scripts/PlayerController.cs
@@ -5,27 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    private Rigidbody rigidbody;
-    private Animator animator;
+    [SerializeField] private Rigidbody rigidbody;
+    [SerializeField]  private Animator animator;
     public float speed = 5;
     public float rotationSpeed = 40;
     // Start is called before the first frame update
-    void Start()
-    {
-        animator = GetComponent<Animator>();
-        rigidbody = GetComponent<Rigidbody>();
-    }
-
     // Update is called once per frame
     void Update()
     {
+        Vector3 directionVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-
-       
-
-        Vector3 directionVector = new Vector3(h,0,v);
         if(directionVector.magnitude > Mathf.Abs(0.05f))
         {
             transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(directionVector), Time.deltaTime * rotationSpeed);
@@ -33,7 +22,5 @@ public class PlayerController : MonoBehaviour
         animator.SetFloat("speed", Vector3.ClampMagnitude(directionVector, 1).magnitude);
 
         rigidbody.velocity = Vector3.ClampMagnitude(directionVector, 1) * speed;
-
-
     }
 }
